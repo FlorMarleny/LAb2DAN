dfc8ef251798a1dca8f50d3bfc94a98d58dde3b9
FlorMarleny <flor.rodriguez@tecsup.edu.pe>
2023-09-05 10:10:51 -0500

    conexion datos

--- conexion.php
+++ conexion.php
@@ -2,11 +2,11 @@
 
 	function conexion(){
 
-	$host = "host=xxxx";
-	$port = "port=xxxx";
-	$dbname = "dbname=xxxx";
-	$user = "user=xxxx";
-	$password = "password=xxxx";
+	$host = "host-containers-us-west-35.railway.app";
+	$port = "port=7124";
+	$dbname = "dbname=railway";
+	$user = "user=postgres";
+	$password = "password=WKcNCGeYCesoJmFdB248";
 
 	$db = pg_connect("$host $port $dbname $user $password");
 
